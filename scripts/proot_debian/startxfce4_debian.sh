529b0398928f3b5cb663cc7c3f44e6bdb4771954
MR.WOODIER <arparnasing@gmail.com>
2025-03-26 22:53:56 +0530

    Update startxfce4_debian.sh

--- scripts/proot_debian/startxfce4_debian.sh
+++ scripts/proot_debian/startxfce4_debian.sh
@@ -6,22 +6,37 @@ kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 # Enable PulseAudio over Network
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 
-# Prepare termux-x11 session
+# Start Virgl Test Server
+virgl_test_server_android &
+
+# Prepare Termux-X11 session
 export XDG_RUNTIME_DIR=${TMPDIR}
 termux-x11 :0 >/dev/null &
 
-# Wait a bit until termux-x11 gets started.
+# Wait a bit until Termux-X11 gets started.
 sleep 3
 
 # Launch Termux X11 main activity
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
-# Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
-# and run XFCE4 as user droidmaster.
-# See also: https://github.com/termux/proot-distro
-# Argument -- acts as terminator of proot-distro login options processing.
-# All arguments behind it would not be treated as options of PRoot Distro.
-proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 startxfce4"'
+# Set up Virgl environment
+export DISPLAY=:0
+export LIBGL_ALWAYS_SOFTWARE=0
+export MESA_LOADER_DRIVER_OVERRIDE=virgl
+export GALLIUM_DRIVER=virpipe
+export MESA_GL_VERSION_OVERRIDE=4.0
+
+# Login to Debian Proot and start XFCE4 with Virgl
+proot-distro login debian --shared-tmp -- /bin/bash -c '
+    export PULSE_SERVER=127.0.0.1
+    export XDG_RUNTIME_DIR=${TMPDIR}
+    export DISPLAY=:0
+    export LIBGL_ALWAYS_SOFTWARE=0
+    export MESA_LOADER_DRIVER_OVERRIDE=virgl
+    export GALLIUM_DRIVER=virpipe
+    export MESA_GL_VERSION_OVERRIDE=4.0
+    su - root -c "env DISPLAY=:0 startxfce4"
+'
 
 exit 0
