29176d7b6c0207cf96213b0057f83ad95aea98ad
MR.WOODIER <arparnasing@gmail.com>
2025-04-05 22:03:37 +0530

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,11 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
 # Kill open X11 processes
 kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 
 # Enable PulseAudio over Network
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 
+#start virgl server wait 
+virgl_test_server_android &
 # Prepare termux-x11 session
 export XDG_RUNTIME_DIR=${TMPDIR}
 termux-x11 :0 >/dev/null &
